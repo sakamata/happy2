151f2633978418617f635a457a263b03bb487ffa
Kazushi Takamura <sakamata@pd5.so-net.ne.jp>
2016-01-24 03:26:50 +0900

    followボタンのstyle修正

--- views/status/main_user.php
+++ views/status/main_user.php
@@ -2,7 +2,7 @@
 	<a href="#">?</a><br>
 	<span>ON/OFF</span><br>
 	<!-- ifFollowing Follower 自分画面では不要 -->
-	<span>Following: <?php echo $this->escape($headerUser['ifFollowing']); ?></span>
+	<span>Following: <?php echo $this->escape($headerUser['ifFollowing']); ?></span><br>
 	<?php if ($headerUser['ifFollowing'] == 0){ ?>
 		<button type="submit" class="btn btn-warning btn-lg">Follow</button>
 	<?php } else { ; ?>
@@ -12,7 +12,7 @@
 
 	<span>Follower: <?php echo $this->escape($headerUser['ifFollower']); ?></span><br>
 	<img src="<?php echo $base_url .'/../img/'. $headerUser['usImg']; ?>" alt="user_photo">
-	img:<?php echo $this->escape($headerUser['usImg']); ?><br>
+	<br>
 	<b>
 	Pt: <?php echo $this->escape($headerUser['nowPt']); ?><br>
 	No: <?php echo $this->escape($headerUser['usNo']); ?><br>
@@ -20,18 +20,3 @@
 	Name: <?php echo $this->escape($headerUser['usName']); ?><br>
 	TodayClick  ForYou / All: <?php echo $this->escape($headerUser['thisUserToMeClkSum']); ?> / <?php echo $this->escape($headerUser['thisUserAllClkSum']); ?><br>
 	</b>
-
-	<h1 id="title">サンプル</h1>
-	<p id="msg">ボタンをクリックしてください。</p>
-	<br /><br />
-	<input type="button" onclick="doAction()" value="クリック" />
-
-
-	<script type="text/javascript">
-	<!--
-
-	function doAction(){
-		$("#msg").text("クリックしました！");
-	}
-	//-->
-	</script>
