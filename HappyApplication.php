fd2455fa9f53dda744d272cabba690af25d8c763
Kazushi Takamura <sakamata@pd5.so-net.ne.jp>
2016-08-22 00:22:37 +0900

    Host Name and info.php pash switching add.

--- HappyApplication.php
+++ HappyApplication.php
@@ -42,7 +42,24 @@ class HappyApplication extends Application
 
 	protected function configure()
 	{
-		require 'C:xampp/htdocs/hidden/info.php';
+		switch($_SERVER["HTTP_HOST"]){
+			case "localhost":
+				$path = 'C:xampp/htdocs/hidden/info.php';
+				break;
+
+			case "160.16.57.194":
+				$path = '/var/www/html/info.php';
+				break;
+
+			case "happy-project.org":
+				$path = '/var/www/html/info.php';
+				break;
+
+			default:
+				$path = 'C:xampp/htdocs/hidden/info.php';
+		};
+
+		require $path;
 
 		$this->db_manager->connect('master', array(
 			'dsn' => $dsn,
