4ed43cb0e9a0d996809405c65b922532c60de41a
Kazushi Takamura <sakamata@pd5.so-net.ne.jp>
2017-10-02 14:43:51 +0900

    htmlページを差し替え

--- web/admin/signin.php
+++ web/admin/signin.php
@@ -1,12 +1,22 @@
 <?php
 
-require_once __DIR__ . '/functions.php';
-$username = require_basic_auth();
+// require_once __DIR__ . '/functions.php';
+// $username = require_basic_auth();
 
-header('Content-Type: text/html; charset=UTF-8');
+// header('Content-Type: text/html; charset=UTF-8');
 
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <title>会員限定ページ</title>
-<h1>ようこそ,<?=h($username)?>さん</h1>
-<a href="http://dummy@localhost:8080/">ログアウト</a>
+<h1>ようこそ,<?//=h($username)?>さん</h1>
+<a href="http://dummy@localhost:8080/">ログアウト</a> -->
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Basic認証test</title>
+	</head>
+	<body>
+		<p>会員限定ページです。</p>
+	</body>
+</html>
